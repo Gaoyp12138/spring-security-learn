2446d8dbcdbb9304ce1f8bc4070c984a290361fa
unknown <gaoyp@yahoo.com>
2018-10-26 17:52:28 +0800

    first commit

---
+++ src/main/java/com/security/ggg/SpringSecurityLearnApplication.java
@@ -0,0 +1,12 @@
+package com.security.ggg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLearnApplication.class, args);
+	}
+}
